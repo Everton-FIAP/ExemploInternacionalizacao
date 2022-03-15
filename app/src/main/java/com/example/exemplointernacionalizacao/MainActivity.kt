a9f18465c92d087fee7138852616f566957e2d51
Everton-FIAP <rm88475@fiap.com.br>
2022-03-14 22:18:14 -0300

    feat: add views to strings.xml

--- app/src/main/java/com/example/exemplointernacionalizacao/MainActivity.kt
+++ app/src/main/java/com/example/exemplointernacionalizacao/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.exemplointernacionalizacao
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val txtNome = findViewById<EditText>(R.id.editTextTextPersonName)
+        val btnMsg = findViewById<Button>(R.id.button)
+
+        btnMsg.setOnClickListener{
+            val msg = "Olá, ${getString(R.string.str_msg)}, ${txtNome.text}"
+            Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+        }
     }
 }
